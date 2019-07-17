e9b9a0a20e55b8e63f658eab8d5ea5a0e1f62b2f
badari <badari0793@gmail.com>
2019-07-17 19:04:08 +0530

    webpack configur

---
+++ src/componets/ClientApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Mytitle from './Mytitle';
+
+
+const App = () => (
+  <div className='App'>
+    <h3><Mytitle title='this is great learn' color='red' /></h3>
+    <h3><Mytitle title='this is bad' color='green' /></h3>
+
+  </div>
+)
+
+export default App;
---
+++ src/componets/Mytitle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Mytitle extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1 style={{ color: this.props.color }}>{this.props.title}</h1>
+      </div>
+
+    )
+  }
+}
+
+export default Mytitle;
