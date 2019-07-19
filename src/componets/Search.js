e4449ccc2939527077919934ff6b7b1c98426ba1
badari <badari0793@gmail.com>
2019-07-19 17:14:44 +0530

    search setup

--- src/componets/Search.js
+++ src/componets/Search.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import data from '../../public/data'
 import ShowCard from './ShowCard'
+class Search extends React.Component {
+  // constructor (props) {
+  //   super(props)
+  //   this.state = {
+  //     searchTerm: 'this is search tearm'
+  //   }
+  //   this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+  // }
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+  // handleSearchTermEvent (e) {
+  //   this.setState({ searchTerm: e.target.value })
+  // }
+  state = {
+    searchTerm: ''
+  }
+
+  handleSearchTermEvent = (e) => this.setState({ searchTerm: e.target.value })
+
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input onChange={this.handleSearchTermEvent} value={this.state.searchTerm} className='search-input' type='text' placeholder='search' />
+        </header>
+        <div className='shows'>
+          {data.shows
+          .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+          .map((show) => (
+            <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Search
