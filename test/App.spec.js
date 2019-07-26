e3f54380e9b0d54e93c7805b373561b7fcab6437
badari <badari0793@gmail.com>
2019-07-26 23:30:40 +0530

    testing mount nyc coverage

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,37 @@
 /*eslint-env mocha */
 
-const { expect } = require('chai')
+// const React = require('react')
+// const Search = require('../src/componets/Search')
+// const  { expect } = require('chai')
+// const  { shallow } = require('enzyme')
+import React from 'react'
+import Search from '../src/componets/Search'
+import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+import { shows }  from '../public/data'
+import showcard from '../src/componets/ShowCard'
+
+
 
 describe('<search/>', () => {
-  it('should pass', () => {
-    expect(1 + 1 ===3).to.be.true
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    // console.log(wrapper.debug())
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render as many shows as there are data for', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(showcard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.getDOMNode().value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })
+
--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,6 +1,18 @@
 import 'babel-register'
 import 'babel-polyfill'
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-global.document = require('jsdom').jsdom('<body><div id="app"></div></body>')
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+var jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
+const { document } = (new JSDOM('<body><div id="app"></div></body>')).window
+
+// global.document = require('jsdom').jsdom('<body><div id="app"></div></body>')
+global.document = document
 global.window = document.defaultView
 global.navigator = window.navigator
